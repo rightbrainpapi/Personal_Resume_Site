5c57352bf32518a286a9b491bccc5d9d607306f1
rightbrainpapi <dahuemaen@gmail.com>
2020-01-26 00:46:48 -0500

    adding animation on scroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
 console.log("We are here")
 
+// var logocon = document.getElementsByClassName("logocon");
 
+// logocon
+
+// function myFunction() {
+//     console.log("hello World");
+//     logocon.animate
+    
+//   }
+
+  
 // skrollr.init();
 // $(".logocon").click(function(){
 //   $("html, body").animate({ scrollTop: 1000 });
 // });
+
+
+function myMove() {
+    var elem = document.getElementById("myAnimation");   
+    var pos = 100;
+    var id = setInterval(frame,5);
+    function frame() {
+      if (pos == 30) {
+        clearInterval(id);
+      } 
+      else {
+        pos--; 
+        elem.style.height = pos + 'vh'; 
+      //   elem.style.height = pos + 'vh'; 
+      }
+    }
+  }
 
 
