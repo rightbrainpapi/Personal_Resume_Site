2ccc2219f5f7b783004ec73e126f0d76e88cba8f
rightbrainpapi <dahuemaen@gmail.com>
2020-02-22 20:02:05 -0500

    adding the media screen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 /////////////////////////////
 ///////IFFE Initializer//////
 /////////////////////////////
-(function (){ 
-    getViewport()
-    })();
+(function() {
+	getViewport();
+})();
 
 /////////////////////////////
 ///////Global Variables//////
@@ -11,14 +11,13 @@
 
 var viewPortWidth;
 var viewPortHeight;
-var myAnimation = document.getElementById("myAnimation"); 
-var cv = document.getElementById('resume');
+var myAnimation = document.getElementById("myAnimation");
+var cv = document.getElementById("resume");
 var hint = document.getElementById("hinter");
 
 var contact_info = document.getElementById("contact_info");
 // var skills = document.getElementById("skill_set");
 var skills = document.getElementById("skill_set");
-
 
 ///////////////////////////////////////
 ///////////////////////////////////////
@@ -27,28 +26,28 @@ var skills = document.getElementById("skill_set");
 ///////////////////////////////////////
 
 function getViewport() {
-    // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-    if (typeof window.innerWidth != 'undefined') {
-      viewPortWidth = window.innerWidth,
-      viewPortHeight = window.innerHeight
-    }
-   
-   // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-    else if (typeof document.documentElement != 'undefined'
-    && typeof document.documentElement.clientWidth !=
-    'undefined' && document.documentElement.clientWidth != 0) {
-       viewPortWidth = document.documentElement.clientWidth,
-       viewPortHeight = document.documentElement.clientHeight
-    }
-   
-    // older versions of IE
-    else {
-      viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
-      viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
-    }
-    return [viewPortWidth, viewPortHeight];
-   }
+	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+	if (typeof window.innerWidth != "undefined") {
+		(viewPortWidth = window.innerWidth), (viewPortHeight = window.innerHeight);
+	}
 
+	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+	else if (
+		typeof document.documentElement != "undefined" &&
+		typeof document.documentElement.clientWidth != "undefined" &&
+		document.documentElement.clientWidth != 0
+	) {
+		(viewPortWidth = document.documentElement.clientWidth),
+			(viewPortHeight = document.documentElement.clientHeight);
+	}
+
+	// older versions of IE
+	else {
+		(viewPortWidth = document.getElementsByTagName("body")[0].clientWidth),
+			(viewPortHeight = document.getElementsByTagName("body")[0].clientHeight);
+	}
+	return [viewPortWidth, viewPortHeight];
+}
 
 ////////////////////////////////////////
 ////////////////////////////////////////
@@ -57,46 +56,42 @@ function getViewport() {
 ////////////////////////////////////////
 
 function animationScroller() {
-    var animationHeight = myAnimation.offsetHeight;
-    // Checking if heights are equal
-    if ((viewPortHeight === animationHeight) || (viewPortHeight < animationHeight)){
-        // SCROLL UP
-        var pos = 100; // start scrolling with animation at 100 (100% of the screens vh)
-        var id = setInterval(frame,15);
-        function frame() {
-        if (pos == 10) {
-            clearInterval(id); // once pos is equal to 10 stop the scrolling
-        } 
-        else {
-            pos--;  // subtract 1 from pos until pos is equal to 10
-            myAnimation.style.height = pos + '%';
-        }
-        }
+	var animationHeight = myAnimation.offsetHeight;
+	// Checking if heights are equal
+	if (viewPortHeight === animationHeight || viewPortHeight < animationHeight) {
+		// SCROLL UP
+		var pos = 100; // start scrolling with animation at 100 (100% of the screens vh)
+		var id = setInterval(frame, 15);
+		function frame() {
+			if (pos == 10) {
+				clearInterval(id); // once pos is equal to 10 stop the scrolling
+			} else {
+				pos--; // subtract 1 from pos until pos is equal to 10
+				myAnimation.style.height = pos + "%";
+			}
+		}
 
-        // Toggling the visibility of the resume and the enter hint
-        elementsToggler(cv, hint);
-    }
-    else if (viewPortHeight > animationHeight) {
-        // SCROLL DOWN
-        var pos = 0; // start scrolling with animation at 0 (0% of the screens vh)
-        var id = setInterval(frame, 10);
-        function frame() {
-          if (pos == 100) {
-            clearInterval(id); // once pos is equal to 100 stop the scrolling
-          } else {
-            pos++; // add 1 to pos until pos is equal to 100
-            myAnimation.style.height = pos + '%'; 
-          };
-        };
+		// Toggling the visibility of the resume and the enter hint
+		elementsToggler(cv, hint);
+	} else if (viewPortHeight > animationHeight) {
+		// SCROLL DOWN
+		var pos = 0; // start scrolling with animation at 0 (0% of the screens vh)
+		var id = setInterval(frame, 10);
+		function frame() {
+			if (pos == 100) {
+				clearInterval(id); // once pos is equal to 100 stop the scrolling
+			} else {
+				pos++; // add 1 to pos until pos is equal to 100
+				myAnimation.style.height = pos + "%";
+			}
+		}
 
-        // Toggling the visibility of the resume and the enter hint
-        elementsToggler(cv, hint);
-    }
-    else{
-        // Do nothing
-    };
-  }
-
+		// Toggling the visibility of the resume and the enter hint
+		elementsToggler(cv, hint);
+	} else {
+		// Do nothing
+	}
+}
 
 ///////////////////////////////////////
 ///////////////////////////////////////
@@ -104,13 +99,12 @@ function animationScroller() {
 ///////////////////////////////////////
 ///////////////////////////////////////
 
-  function elementsToggler(ele1, ele2) {
+function elementsToggler(ele1, ele2) {
+	// Opens Element 1: Toggles Opacity 1 and visibility visibile
+	ele1.classList.toggle("open");
 
-    // Opens Element 1: Toggles Opacity 1 and visibility visibile 
-    ele1.classList.toggle('open');
-
-    // Closes Element 2: Toggles Opacity 0 and visibility hidden 
-    ele2.classList.toggle('close'); 
+	// Closes Element 2: Toggles Opacity 0 and visibility hidden
+	ele2.classList.toggle("close");
 }
 
 ////////////////////////////////////////////////
@@ -121,8 +115,7 @@ function animationScroller() {
 
 var year = new Date().getFullYear();
 var date = `Copyright &copy; ${year} | Rightbrainpapi.`;
-document.getElementsByClassName('footer')[0].innerHTML = date;
-
+document.getElementsByClassName("footer")[0].innerHTML = date;
 
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
@@ -130,32 +123,34 @@ document.getElementsByClassName('footer')[0].innerHTML = date;
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
 
-
-// function willToggle (element){
-
-//   toggleInfo (element);
-
-// }
-
-
 // Toggle element visibility
 function toggleInfo(elem) {
-  // console.log(skills.target)
-  // console.log(contact_info.target)
-  // elem.classList.toggle('is-visible');
-  var elem = document.getElementById(elem);
-  var resume_content = document.getElementsByClassName("resume_content");
-   if(elem.style.display != "none"){
-    // resume_content.style.display ="flex";
-    elem.style.display = 'none';
-   }else{
-     
-    elem.style.display = 'block';
-   }
-};
+	// console.log(skills.target)
+	// console.log(contact_info.target)
+	// elem.classList.toggle('is-visible');
+	var elem = document.getElementById(elem);
+	var resume_content = document.getElementsByClassName("resume_content");
+	if (elem.style.display != "none") {
+		// resume_content.style.display ="flex";
+		elem.style.display = "none";
+	} else {
+		elem.style.display = "block";
+	}
+}
 
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+//////////////// Portfolio View ////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
 
+const imgContent = document.querySelectorAll(".img-content-hover");
 
+function showImgContent(e) {
+	for (var i = 0; i < imgContent.length; i++) {
+		imgContent[i].style.left = e.pageX + "px";
+		imgContent[i].style.top = e.pageY + "px";
+	}
+}
 
-
-
+document.addEventListener("mousemove", showImgContent);
